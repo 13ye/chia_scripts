4b845f20b9d0f97a522558d7ea04ad91f34fe930
13ye <2559877637@qq.com>
2021-05-17 22:52:40 +0800

    edit

--- use_megacli.sh
+++ use_megacli.sh
@@ -15,16 +15,22 @@ megacli -LDInfo -LAll -aAll
 megacli -PDList –aAll
 # 查看物理设备温度
 megacli pdlist aall | grep "Drive Temperature"
+
 # 查看JBOD是否启用
 megacli -AdpGetProp -enablejbod -aALL
 # 所有适配器启用JBOD
-megacli -AdpGetProp -enablejbod 1 -aALL
+megacli -AdpSetProp -enablejbod 1 -aALL
 # 查看设备E:S
 megacli pdlist aall | grep -Ei "Enclosure Device ID|Slot Number"
 # 以下命令为设置适配器0号的一块物理盘E:S为JBOD模式
 # E代表Enclosure Device ID, S代表Slot Number
-megacli -PDMakeJBOD -PhysDrv[E:S] -a0
+# 通配不行的时候在bash而非zsh下运行
+MegaCli -PDMakeJBOD -PhysDrv[E0:S0,E1:S1,...] -aN|-a0,1,2|-aALL
 # 查看设备是否被设置为JBOD模式
 megacli PDList -aALL -Nolog|grep '^Firm'
 
+# 将一块盘上线
+MegaCli -PDOnline  -PhysDrv[E0:S0,E1:S1,...] -aN|-a0,1,2|-aALL
 
+# 清除所有Ld配置
+megacli -CfgLdDel -LAll -aAll
